2342ac43819554c0b08cf7e9947eeed69caeb2e2
Rizki Fadil <rixkifadil@gmail.com>
2026-03-13 16:43:48 +0700

    menambahkan eventlistener tambahProdi_click() pada button tambah prodi

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -161,7 +161,29 @@ namespace PraktikumADO
             }
         }
 
-        
+        private void TambahProdi_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "INSERT INTO ProgramStudi VALUES ('MI01','Manajemen Informatika')";
+                cmd = new SqlCommand(query, conn);
+
+                int hasil = cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Jumlah baris terpengaruh : " + hasil);
+
+                conn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
     }
 
