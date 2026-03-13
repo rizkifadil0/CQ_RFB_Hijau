77886b9db1c97d5606c7dc698d5f67ca89bd405a
Rizki Fadil <rixkifadil@gmail.com>
2026-03-13 16:39:55 +0700

    menambahkan eventlistener pada hitungdosen_click()

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -117,7 +117,28 @@ namespace PraktikumADO
             }
         }
 
-       
+        private void HitungDosen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "SELECT COUNT(*) FROM Dosen";
+                cmd = new SqlCommand(query, conn);
+
+                int jumlah = (int)cmd.ExecuteScalar();
+
+                txtHasil.Text = jumlah.ToString();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        
 
     }
 
