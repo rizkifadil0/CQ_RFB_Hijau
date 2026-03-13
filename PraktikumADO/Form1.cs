748b71b998f4c1153a9e4d2834027af30b57277f
Rizki Fadil <rixkifadil@gmail.com>
2026-03-13 16:37:28 +0700

    menambahkan eventlistener update_CLick()

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -73,6 +73,52 @@ namespace PraktikumADO
             }
         }
 
+        private void HitungMk_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "SELECT COUNT(*) FROM MataKuliah";
+                cmd = new SqlCommand(query, conn);
+
+                int jumlah = (int)cmd.ExecuteScalar();
+
+                txtHasil.Text = jumlah.ToString();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "UPDATE Mahasiswa SET Alamat='Yogayakarta' WHERE NIM='23110100001'";
+                cmd = new SqlCommand(query, conn);
+
+                int hasil = cmd.ExecuteNonQuery();
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+       
+
     }
 
 }
