01310aa16cafc67576c7fd2e252dc61f79b5d67d
Rizki Fadil <rixkifadil@gmail.com>
2026-03-13 16:35:30 +0700

    menambahkan hitungMHS_click

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -50,7 +50,28 @@ namespace PraktikumADO
             }
         }
 
-        
+        private void HitungMhs_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "SELECT COUNT(*) FROM Mahasiswa";
+
+                cmd = new SqlCommand(query, conn);
+
+                int jumlah = (int)cmd.ExecuteScalar();
+
+                txtHasil.Text = jumlah.ToString();
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
     }
 
